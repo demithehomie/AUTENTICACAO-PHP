3b5b50c0f45f3ead2ce992dd94f322e36951247b
TÉCNICO EM INFORMÁTICA <51127512022.2@senacrjedu.local>
2022-12-05 11:26:05 -0300

    login com estilizacao

---
+++ login.php
@@ -0,0 +1,17 @@
+
+
+<form method="post">
+    <div style="justify-content: center; align-items: center; display: flex;">
+        <div style="width: 200px; height: 200px; background-color: blue; color: #fff; padding: 30px; border-radius: 30px; ">    
+    <h1>Receba!</h1>
+    
+    <input type="text" name="login" placeholder="INSIRA SEU LOGIN"/>
+    <br>
+    <br>
+    <input type="password" name="senha" placeholder="INSIRA SUA SENHA"/>
+    <br>
+    <br>
+    <input type="submit" name="acao" value="Enviar"/>
+        </div>
+    </div>
+</form>
